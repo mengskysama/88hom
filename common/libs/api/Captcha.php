34a981204b3d9a3732ad217739bb05a6f9619a74
unknown <cheneil@itprc-30.midland.local>
2013-05-24 18:26:36 +0800

    verify code

--- common/libs/api/Captcha.php
+++ common/libs/api/Captcha.php
@@ -1,11 +1,4 @@
 <?php  
-/* 
- *  $Captcha = new Captcha(); 
- *  //exit($Captcha->get_checkcode()); 
- *  $Captcha->set_checkimage_wh(60, 20); 
- *  $Captcha->out_checkimage(); 
- */  
-  
 class Captcha {  
     private $mCheckMode = 'number';     //验证模式：'number': 数字 'add': 加法  
     private $mCheckCodeNum = 4;         //验证码位数  
@@ -74,7 +67,8 @@ class Captcha {
     public function out_checkimage() {  
         $this->out_fileheader();  
         $this->create_image();  
-        $this->set_disturb_color();  
+        //$this->set_disturb_color();  
+        $this->set_sin_line();
         $this->write_checkcode_to_image();  
         imagepng($this->mCheckImage);  
         imagedestroy($this->mCheckImage);  
@@ -85,7 +79,7 @@ class Captcha {
      * @brief 输出头 
      */  
     private function out_fileheader() {  
-        header ("Content-type: image/png");  
+        header ("Content-type: image/png");
     }  
   
   
@@ -97,7 +91,7 @@ class Captcha {
             $this->mCheckCode = strtoupper(substr(md5(rand()), 0, $this->mCheckCodeNum));  
         } else {  
             $num1 = rand(10, 49);  
-            $num2 = rand(10, 49);  
+            $num2 = rand(1, 9);  
             $this->mCheckResult = $num1 + $num2;  
             $this->mCheckCode = $num1 .'+'. $num2 .'=';  
             $this->set_checkcode_num(strlen($this->mCheckCode));  
@@ -111,8 +105,8 @@ class Captcha {
      */  
     private function create_image() {  
         $this->mCheckImage = @imagecreate ($this->mCheckImageWidth, $this->mCheckImageHeight);  
-        $bgcolor = imagecolorallocate ($this->mCheckImage, 245,245,245);     //背景色  
-        $iborder = imagecolorallocate ($this->mCheckImage, 206,206,206);     //边框色  
+        $bgcolor = imagecolorallocate ($this->mCheckImage, 255,255,255);     //背景色  
+        $iborder = imagecolorallocate ($this->mCheckImage, 255,255,255);     //边框色  
         imagerectangle($this->mCheckImage, 0, 0, $this->mCheckImageWidth-1, $this->mCheckImageHeight-1, $iborder);  
         return $this->mCheckImage;  
     }  
@@ -127,16 +121,36 @@ class Captcha {
             imagesetpixel($this->mCheckImage, rand(2, 128), rand(2, 38), $this->mDisturbColor);  
         }  
     }  
-  
+    //画正弦干扰线
+    private function set_sin_line() {
+    	$img = $this->mCheckImage;
+
+    	$w=$this->mCheckImageWidth - 20;
+    	$h=$this->mCheckImageHeight;
+    	$h1=rand(-5,5);
+    	$h2=rand(-1,1);
+    	$w2=rand(10,15);
+    	$h3=rand(4,6);
+    	$color = imagecolorallocate ($this->mCheckImage,0,0,0);
+    
+    	for($i=-$w/2;$i<$w/2;$i=$i+0.1)
+    	{
+    		$y=$h/$h3*sin($i/$w2)+$h/2+$h1;
+    		imagesetpixel($img,$i+$w/2,$y,$color);
+    		$h2!=0?imagesetpixel($img,$i+$w/2,$y+$h2,$color):null;
+    	}
+    }
   
     /** 
      * @brief 在验证码图片上逐个画上验证码 
      */  
     private function write_checkcode_to_image() {  
+    	
         for ($i = 0;$i <= $this->mCheckCodeNum;$i++) {  
             $bg_color = imagecolorallocate ($this->mCheckImage, rand(0, 255), rand(0, 128), rand(0, 255));  
-            $x = floor($this->mCheckImageWidth / $this->mCheckCodeNum) * $i + 3;  
-            $y = rand(0, $this->mCheckImageHeight-15);  
+            $x = ($i == 0) ? 1 : floor($this->mCheckImageWidth / $this->mCheckCodeNum) * $i - 4 ;  
+            $y = rand(0, $this->mCheckImageHeight-15); 
+            
             imagechar ($this->mCheckImage, 5, $x, $y, $this->mCheckCode[$i], $bg_color);  
         }  
     }  
--- ucenter/test.php
+++ ucenter/test.php
@@ -1,15 +1,7 @@
 <?php
-header ("Content-type: image/png");
-
-$mCheckImage = @imagecreate (60, 20);
-$bgcolor = imagecolorallocate ($mCheckImage, 245,245,245);     //背景色
-$iborder = imagecolorallocate ($mCheckImage, 206,206,206);     //边框色
-imagerectangle($this->mCheckImage, 0, 0, $this->mCheckImageWidth-1, $this->mCheckImageHeight-1, $iborder);
-/*
-require 'E:/workspace/projects/88hom/common/libs/api/Captcha.php';
-$Captcha = new Captcha(); 
+require '../common/libs/api/Captcha.php';
+$Captcha = new Captcha("add"); 
 $Captcha->set_checkimage_wh(60, 20); 
+$Captcha->set_checkcode_num(4);
 $Captcha->out_checkimage();
-*/
- 
 ?>
